5f408b20f9b569332c3755cd28b9a2bd1198e31e
Colin Gregurich <coling112198@gmail.com>
2022-10-08 20:48:55 -0500

    created components

---
+++ src/components/FormButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <button
+    class="login-btn bg-black text-white rounded-lg text-lg transition-none w-32 h-12 hover:bg-gray-800 active:bg-gray-700 
+    disabled:bg-gray-400 disabled:text-gray-500"
+    @click="$emit('click')"
+    :disabled="disabled"
+  >
+    {{ text }}
+  </button>
+</template>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, watch } from 'vue';
+const props = defineProps({
+  labelText: String,
+  enteredText: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  isPassword: {
+    type: Boolean,
+    default: false,
+  },
+  icon: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
+
+<template>
+  <div class="input-container flex flex-col w-4/5 mb-6 relative">
+    <font-awesome-icon v-if="icon" :icon="icon" class="absolute top-8 left-2" />
+    <label>{{ labelText }}</label>
+    <input
+      class="box-border rounded-md border border-black pl-8 py-1"
+      :type="isPassword ? 'password' : 'text'"
+      :value="enteredText"
+      @input="$emit('update:enteredText', $event.target.value)"
+    >
+  </div>
+  
+</template>
