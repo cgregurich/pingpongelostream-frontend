e4c8d587747d1fafc4c65719eda11313e3bba4a1
Colin Gregurich <coling112198@gmail.com>
2022-10-08 20:48:00 -0500

    put router setup in its own module, added font awesome

--- src/main.js
+++ src/main.js
@@ -2,37 +2,21 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
-import Sandbox from '@/components/Sandbox.vue';
-import Home from '@/components/Home.vue';
-import PlayMatch from '@/components/PlayMatch.vue';
-import Leaderboard from '@/components/Leaderboard.vue';
-import About from '@/components/About.vue';
-import Profile from '@/components/Profile.vue';
-import Example from '@/components/Example.vue';
 
-import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/sandbox', component: Sandbox, name: 'Sandbox'},
-  { path: '/leaderboard', component: Leaderboard, name: 'Leaderboard' },
-  { path: '/about', component: About, name: 'About'},
-  { path: '/profile', component: Profile, name: 'Profile'},
-  { path: '/play', component: PlayMatch, name: 'Play'},
-  { path: '/example', component: Example, name: 'Example'},
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
-
+import router from '@/router';
 
 const pinia = createPinia();
 
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faLock, faTableTennisPaddleBall } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUser, faLock, faTableTennisPaddleBall);
 
 createApp(App)
   .use(router)
   .use(pinia)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
