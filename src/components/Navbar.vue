d57a2f9b129aae6748d2315acfb119ad6d7453f9
Colin Gregurich <coling112198@gmail.com>
2022-10-15 21:38:32 -0500

    removed js styles logic

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,14 +8,12 @@ const navItems = ref([
   { text: 'Log In', routeName: 'Login' },
   { text: 'Register', routeName: 'Register' },
   ]);
-// const bg = ref('bg-emerald-600');
-// const fg = ref('text-white');
 </script>
 
 
 <template>
-<nav class="navbar flex items-center py-2" :class="bg">
-  <div class="logo flex items-center text-2xl font-bold" :class="[fg, bg]">
+<nav class="navbar flex items-center py-2 bg-site-background text-background">
+  <div class="logo flex items-center text-2xl font-bold">
     <font-awesome-icon icon="fa-table-tennis-paddle-ball" class="pl-4 pr-1" />
     Attic Ping Pong
   </div>
@@ -23,7 +21,6 @@ const navItems = ref([
     v-for="(navItem, index) in navItems"
     :key="index"
     class="mx-6 px-6 text-lg w-max border-0 border-black"
-    :class="fg"
     active-class="border-b-4 border-black"
     :to="{ name: navItem.routeName }"
   >
