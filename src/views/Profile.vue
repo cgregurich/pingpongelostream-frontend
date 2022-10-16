a55ed850b6a43521e1119c92017b2a0647605bb0
Colin Gregurich <coling112198@gmail.com>
2022-10-15 21:40:27 -0500

    added tailwind color aliases

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -55,44 +55,41 @@
 	const match2 = ref(matchData2);
 	const match3 = ref(matchData3);
 	
-	// const bg = ref('bg-emerald-700');
-	// const text = ref('text-white');
-	// const textMuted = ref('text-emerald-100');
 </script>
 
 <template>
-	<body class="flex flex-col items-center" :class="bg">
+	<body class="flex flex-col items-center bg-site-background">
 		<div class="header flex items-center mt-4">
 			<img class="profile-pic rounded-full w-44 shadow-2xl" src="https://xsgames.co/randomusers/assets/avatars/male/27.jpg">
-			<div class="username ml-4 font-semibold text-2xl" :class="text">david.j123</div>
+			<div class="username ml-4 font-semibold text-2xl text-primary">david.j123</div>
 		</div>
 		<div class="stats flex justify-around w-3/6 mt-8">
 			<div class="stat-container flex flex-col items-center justify-center text-center">
-				<div class="stat-data font-semibold text-2xl" :class="text">#3</div>
-				<div class="stat-label" :class="textMuted">Current Ranking</div>
+				<div class="stat-data font-semibold text-2xl text-primary">#3</div>
+				<div class="stat-label">Current Ranking</div>
 			</div>
 			<div class="stat-container flex flex-col items-center justify-center text-center">
-				<div class="stat-data font-semibold text-2xl" :class="text">2500</div>
-				<div class="stat-label" :class="textMuted">Current Elo</div>
+				<div class="stat-data font-semibold text-2xl text-primary">2500</div>
+				<div class="stat-label">Current Elo</div>
 			</div>
 			<div class="stat-container flex flex-col items-center text-center">
-				<div class="stat-data font-semibold text-2xl" :class="text">123</div>
-				<div class="stat-label" :class="textMuted">Matches Played</div>
+				<div class="stat-data font-semibold text-2xl text-primary">123</div>
+				<div class="stat-label">Matches Played</div>
 			</div>
 			<div class="stat-container flex flex-col items-center text-center">
-				<div class="stat-data font-semibold text-2xl" :class="text">76%</div>
-				<div class="stat-label" :class="textMuted">Win Rate</div>
+				<div class="stat-data font-semibold text-2xl text-primary">76%</div>
+				<div class="stat-label">Win Rate</div>
 			</div>
 		</div>
 		<div class="recent-matches w-3/6 flex flex-col items-center mt-6">
 			<div class="flex justify-between w-full">
-				<div class="text-lg ml-2" :class="textMuted">Recent Matches</div>
+				<div class="text-lg ml-2 text-primary">Recent Matches</div>
 			</div>
 			<MatchSummaryCard :playerOne="match1.p1" :playerTwo="match1.p2" />
 			<MatchSummaryCard :playerOne="match2.p1" :playerTwo="match2.p2" />
 			<MatchSummaryCard :playerOne="match3.p1" :playerTwo="match3.p2" />
 			<div class="flex justify-end w-full">
-				<div class="text-sm hover:underline cursor-pointer mt-1 mr-2" :class="text">View Complete Match History</div>
+				<div class="text-sm text-primary hover:underline cursor-pointer mt-1 mr-2">View Complete Match History</div>
 			</div>
 		</div>
 	</body>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'play-game-color-one': colors.teal[400],
+        'play-game-color-two': colors.black,
+        'site-background': colors.white,
+        'primary': colors.black,
+        'secondary': colors.green[600],
+        'secondary-muted': colors.red[900]
+      }
+    },
   },
   plugins: [],
 }
