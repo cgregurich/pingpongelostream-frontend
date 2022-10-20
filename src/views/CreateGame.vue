96727fd6146cfeb26e77ace19c31b085ead0e70e
terrahQ <85520440+terrahQ@users.noreply.github.com>
2022-10-20 00:26:49 -0500

    added totally basic index pages

--- src/views/CreateGame.vue
+++ src/views/CreateGame.vue
@@ -1,3 +1,43 @@
+<script>
+export default {
+	data() {
+		return {
+
+
+
+		}
+	}
+}
+
+
+</script>
+
+
 <template>
-    <h1>Create Game</h1>
+  <body>
+    <div class="body flex justify-center w-full">
+      <div
+        class="
+          game-list-container
+          flex flex-col
+          items-center
+          sm:w-[30rem]
+          w-full
+          bg-gray-100
+          rounded-3xl
+          m-4
+          pb-5
+          border border-gray-300
+          shadow-lg
+        "
+      >
+        <p class="header text-4xl font-semibold m-4">Create a Game</p>
+
+        <div
+          class="line w-4/5 lg:mt-40 mt-1 bg-opacity-10 bg-black h-[1px] mb-4"
+        ></div>
+        <div class="footer">GO</div>
+      </div>
+    </div>
+  </body>
 </template>
--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,3 +1,61 @@
+<script>
+export default {
+	data() {
+		return {
+
+			game1: [{ winner: "Jane", loser: "Jim", score: "11 - 4", date: "10 / 4 / 22" }],
+			game2: [{ winner: "Jack", loser: "Josephine", score: "11 - 9", date: "10 / 1 / 22" }],
+			game3: [{ winner: "Job", loser: "Jerry", score: "13 - 11", date: "10 / 14 / 22" }],
+			game4: [{ winner: "Jeckyl", loser: "Jaquelynn", score: "11 - 7", date: "10 / 7 / 22" }],
+			game5: [{ winner: "John", loser: "Johnny", score: "12 - 10", date: "10 / 16 / 22" }],
+
+		}
+	}
+}
+
+
+</script>
+
+
 <template>
-  <h1>Leaderboard</h1>
+  <body>
+    <div class="body flex justify-center w-full">
+      <div
+        class="
+          game-list-container
+          flex flex-col
+          items-center
+          sm:w-[30rem]
+          w-full
+          bg-gray-100
+          rounded-3xl
+          m-4
+          pb-5
+          border border-gray-300
+          shadow-lg
+        "
+      >
+        <p class="header text-4xl font-semibold m-4">ELO Leaderboard</p>
+        <div v-for="item in game1">
+          {{ item.winner }}
+        </div>
+		<div v-for="item in game2">
+          {{ item.winner }}
+        </div>
+		<div v-for="item in game3">
+          {{ item.winner }}
+        </div>
+		<div v-for="item in game4">
+          {{ item.winner }}
+        </div>
+		<div v-for="item in game5">
+          {{ item.winner }}
+        </div>
+        <div
+          class="line w-4/5 lg:mt-40 mt-1 bg-opacity-10 bg-black h-[1px] mb-4"
+        ></div>
+        <div class="footer">Put pagination here</div>
+      </div>
+    </div>
+  </body>
 </template>
