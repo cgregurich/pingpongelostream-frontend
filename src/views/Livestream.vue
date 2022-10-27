29869988ae3072b3981e37890946c986c06e90e2
Alex Wissing <awissing123@gmail.com>
2022-10-27 13:46:32 -0600

    started working on livestream

--- src/views/Livestream.vue
+++ src/views/Livestream.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Livestream</h1>
+    <div id="wrap">
+        <div>
+            <div class="flex justify-center mt-3 player-container">
+                <video-player
+                    :options="videoOptions"
+                ></video-player>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import VideoPlayer from '../components/VideoPlayer.vue';
+
+export default {
+    name: 'Livestream',
+    components: {
+        VideoPlayer
+    },
+    data() {
+        return {
+            videoOptions: {
+                responsive: true,
+                autoplay: true,
+                controls: true,
+                sources: [
+                    {
+                        src: 'http://stream.atticpingpong.com:8088/dash/obs_stream.mpd',
+                        type: 'application/dash+xml'
+                    }
+                ]
+            }
+        };
+    }
+}
+</script>
