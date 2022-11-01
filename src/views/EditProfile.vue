110d1e60823b3be1b177702d226babc81d01e9f7
Alex Wissing <awissing123@gmail.com>
2022-11-01 16:33:15 -0600

    started playing with editprofile page

---
+++ src/views/EditProfile.vue
@@ -0,0 +1,16 @@
+<script setup>
+import UpdateProfileInformationForm from '@/components/UpdateProfileInformationForm.vue';
+import UpdatePasswordForm from '@/components/UpdatePasswordForm.vue';
+import ActiveSessionsForm from '@/components/ActiveSessionsForm.vue';
+import DeleteUserForm from '@/components/DeleteUserForm.vue';
+</script>
+<template>
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <UpdateProfileInformationForm :user="$page.props.user" />
+            <UpdatePasswordForm class="mt-10 sm:mt-0" />
+            <ActiveSessionsForm :sessions="sessions" class="mt-10 sm:mt-0" />
+            <DeleteUserForm class="mt-10 sm:mt-0" />
+        </div>
+    </div>
+</template>
