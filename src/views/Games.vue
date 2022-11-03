689cad8393ea840b1094cd9755c878fcfbc10c00
Colin Gregurich <coling112198@gmail.com>
2022-11-03 14:15:12 -0500

    made simple games page for testing purposes

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,3 +1,32 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref, reactive, computed } from 'vue';
+import { API_URL } from '@/stores/utils/backendRouteParts.js';
+
+
+const games = reactive([]);
+
+onMounted(async () => {
+  const response = await axios.get(`${API_URL}/games`);
+  Object.assign(games, response.data.response.games);
+});
+
+const playedGames = computed(() => games.filter(game => game.started_at != null && game.completed_at != null));
+const unplayedGames = computed(() => games.filter(game => game.started_at == null));
+
+function getPlayersForGame(game) {
+  const teamOne = game.teams[0].members.map(member => member.name);
+  const teamTwo = game.teams[1].members.map(member => member.name);
+  return { teamOne, teamTwo };
+}
+
+</script>
+
 <template>
     <h1>Games</h1>
+    <p>playedGames.length: {{ playedGames.length }}</p>
+    <p>unplayedGames.length: {{ unplayedGames.length }}</p>
+    <div class="game" v-for="game in unplayedGames" :key="game.id">
+      {{ getPlayersForGame(game).teamOne }} vs {{ getPlayersForGame(game).teamTwo }}
+    </div>
 </template>
