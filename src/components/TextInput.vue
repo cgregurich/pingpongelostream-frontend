6cc676038fb8b9148f0d08c39216b3ea1ab0fadc
Colin Gregurich <coling112198@gmail.com>
2022-11-03 14:14:26 -0500

    added placeholder

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -6,17 +6,20 @@ const props = defineProps({
     default: '',
     required: true,
   },
+  placeholder: {
+    type: String,
+    default: ''
+  },
 });
 </script>
 
 
 <template>
-  <div class="input-container flex flex-col w-4/5 relative">
-    <input
-      class="box-border rounded-md active:border-0 focus:border-0 outline-0 px-2 py-1"
-      :value="enteredText"
-      @input="$emit('update:enteredText', $event.target.value)"
-    >
-  </div>
+  <input
+    class="box-border rounded-md border border-black px-2 py-1"
+    :value="enteredText"
+    :placeholder="placeholder"
+    @input="$emit('update:enteredText', $event.target.value)"
+  >
   
 </template>
