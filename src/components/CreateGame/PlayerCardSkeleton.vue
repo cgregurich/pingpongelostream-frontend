b9902567f8d016fe80ef1fb27d47e4c170c20c66
Colin Gregurich <coling112198@gmail.com>
2022-11-03 14:04:20 -0500

    styling

--- src/components/CreateGame/PlayerCardSkeleton.vue
+++ src/components/CreateGame/PlayerCardSkeleton.vue
@@ -1,5 +1,5 @@
 
 <template>
-  <div class="player-card flex justify-start items-center w-content bg-white opacity-40 border-2 border-gray-300 rounded-md mx-4 px-4 py-2 my-4 w-[300px] select-none h-[92px]">
+  <div class="player-card flex justify-start items-center w-content bg-site-color-two opacity-30 border-2 border-gray-300 rounded-md mx-4 px-4 py-2 my-4 w-[300px] select-none h-[84px]">
   </div>
 </template>
