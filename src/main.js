6731ffd5dce29961e468dd77f2eed78e75cd3417
Colin Gregurich <coling112198@gmail.com>
2022-11-03 13:56:27 -0500

    changed icons

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import router from '@/router';
 const pinia = createPinia();
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faLock, faTableTennisPaddleBall, faArrowRotateLeft, faTrash, faArrowLeft, faArrowRight, faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faLock, faTableTennisPaddleBall, faArrowRotateLeft, faTrashAlt, faArrowLeft, faArrowRight, faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUser, faLock, faTableTennisPaddleBall, faArrowRotateLeft, faTrash, faArrowLeft, faArrowRight, faLongArrowAltLeft, faLongArrowAltRight);
+library.add(faUser, faLock, faTableTennisPaddleBall, faArrowRotateLeft, faTrashAlt, faArrowLeft, faArrowRight, faLongArrowAltLeft, faLongArrowAltRight);
 
 createApp(App)
   .use(pinia)
