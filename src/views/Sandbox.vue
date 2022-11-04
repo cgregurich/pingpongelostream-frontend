cee7799697a647564c28a69cb7727b9cf09bbd7d
Colin Gregurich <coling112198@gmail.com>
2022-11-04 10:02:40 -0500

    wip commit

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,17 +1,9 @@
 <script setup>
-// import axios from 'axios';
-// import { ref, reactive } from 'vue';
-
-// async function asyncFunc() {
-//   const response = await axios.get('http://pingpong.test/api/players/2');
-//   const player = response.data.response.player;
-//   return player;
-// }
-
-
-// const player = ref(await asynFunc());
+import Loader from '@/components/Loader.vue';
+import PrimaryButton from '@/components/PrimaryButton.vue';
 </script>
 
 <template>
-
+  <PrimaryButton text="hello" class="animate-bounce">
+  </PrimaryButton>
 </template>
