96f4ead583e5e53f9c1050c89ddbfdada699fe25
Colin Gregurich <coling112198@gmail.com>
2022-11-04 14:00:19 -0500

    simple page to test stuf

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -2,8 +2,9 @@
 import axios from 'axios';
 import { onMounted, ref, reactive, computed } from 'vue';
 import { API_URL } from '@/stores/utils/backendRouteParts.js';
+import { useRouter } from 'vue-router';
 
-
+const router = useRouter();
 const games = reactive([]);
 
 onMounted(async () => {
@@ -15,18 +16,23 @@ const playedGames = computed(() => games.filter(game => game.started_at != null 
 const unplayedGames = computed(() => games.filter(game => game.started_at == null));
 
 function getPlayersForGame(game) {
-  const teamOne = game.teams[0].members.map(member => member.name);
-  const teamTwo = game.teams[1].members.map(member => member.name);
-  return { teamOne, teamTwo };
+  const playerOne = game.teams[0].members.map(member => member.name)[0];
+  const playerTwo = game.teams[1].members.map(member => member.name)[0];
+  return { playerOne, playerTwo };
 }
 
+
+const singlesGames = computed(() => unplayedGames.value.filter(game => game.teams[0].members.length === 1 && game.teams[1].members.length === 1));
+console.log(unplayedGames.value);
 </script>
 
 <template>
     <h1>Games</h1>
     <p>playedGames.length: {{ playedGames.length }}</p>
     <p>unplayedGames.length: {{ unplayedGames.length }}</p>
-    <div class="game" v-for="game in unplayedGames" :key="game.id">
-      {{ getPlayersForGame(game).teamOne }} vs {{ getPlayersForGame(game).teamTwo }}
+    <div class="game" v-for="game in unplayedGames" :key="game.id" @click="router.push({ name: 
+    'Play', params: { gameID: game.id } })">
+      {{ game.id }} : {{ getPlayersForGame(game).playerOne }} vs {{ getPlayersForGame(game).playerTwo }}
+      
     </div>
 </template>
