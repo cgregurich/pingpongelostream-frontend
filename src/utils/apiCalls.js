f41a67cb85d899dfe96f40785aeae967b4a2f925
Colin Gregurich <coling112198@gmail.com>
2022-11-04 12:21:23 -0500

    created utils folder

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { API_URL } from '@/stores/utils/backendRouteParts.js';
+import { useAuthStore } from '@/stores/modules/auth';
+
+const authStore = useAuthStore();
+
+export async function getAllPlayers() {
+  const response = await axios.get(`${API_URL}/players`);
+  if (response.status === 200) return response.data.response.players;
+  else return null;
+}
+
+export async function createGame(gameData) {
+  const config = {
+    headers: authStore.bearerToken
+  };
+  const response = await axios.post(`${API_URL}/games`, gameData, config);
+  return response.status === 200;
+}
+
+export async function getPlayerSinglesTeamID(playerID) {
+  const response = await axios.get(`${API_URL}/players/${playerID}/teams/singles`);
+  if (response.status === 200) return response.data.response.team_id;
+  else return null;
+}
---
+++ src/utils/toasts.js
@@ -0,0 +1,19 @@
+import { useToast } from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
+const toast = useToast({ position: 'bottom' });
+
+let creating;
+export function creatingGame() {
+  creating = toast.info('Creating game...', { duration: 0, dismissible: false });
+}
+
+export function createGameFailed() {
+  creating.dismiss();
+  toast.error('Couldn\'t create game', { duration: 2000 });
+}
+
+export function gameCreated() {
+  creating.dismiss();
+  toast.success('Game created!', { duration: 2000 });
+}
