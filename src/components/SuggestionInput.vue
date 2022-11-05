7e362cb932a6ef43bf205a0d02e29db0d6d39b4e
Colin Gregurich <coling112198@gmail.com>
2022-11-05 10:56:49 -0500

    fixed focus issue

--- src/components/SuggestionInput.vue
+++ src/components/SuggestionInput.vue
@@ -1,7 +1,6 @@
 <script setup>
 import TextInput from '@/components/TextInput.vue';
 import { ref, reactive, computed } from 'vue';
-import { onClickOutside } from '@vueuse/core'
 
 
 const props = defineProps({
@@ -47,14 +46,23 @@ function suggestionClicked(suggestion) {
   emit('update:enteredText', suggestion);
   inputField.value.focus();
 }
+
+const showSuggestions = computed(() => {
+  if (!suggestions.value) return false;
+  // don't show suggestions if the component isn't focused, or if there are no suggestions
+  if (!isFocused.value || suggestions.value?.length === 0) return false;
+  // also don't show if there's only a single suggestion and it matches what is entered
+  if (suggestions.value?.length === 1 && suggestions.value[0].toLowerCase() === props.enteredText.toLowerCase()) return false;
+  return true;  
+});
   
 </script>
 
 <template>
-<div class="search-input relative" @click.prevent="inputField.focus()">
+<div class="search-input relative" @click="inputField.focus()" @mousedown.stop.prevent="">
   <input class="border border-black px-1" type="text" ref="inputField" @focus="isFocused = true" @blur="isFocused = false" v-model="enteredText" @input="$emit('update:enteredText', $event.target.value)">
-  <div v-show="suggestions?.length > 0 && isFocused" class="suggestions absolute w-full bg-white border border-black">
-    <div @click="suggestionClicked(suggestion)" class="suggestion cursor-default" v-for="(suggestion, index) in suggestions" :key="index">{{ suggestion }}</div>
+  <div v-show="showSuggestions" class="suggestions absolute w-full bg-white border border-black">
+    <div @click="suggestionClicked(suggestion)" class="suggestion cursor-default hover:text-white hover:bg-blue-500" v-for="(suggestion, index) in suggestions" :key="index">{{ suggestion }}</div>
   </div>
 </div>
 </template>
