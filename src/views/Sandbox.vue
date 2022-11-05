bcb5df1661cc8c851f44a3e70679a3db078cdbce
Colin Gregurich <coling112198@gmail.com>
2022-11-05 17:00:07 -0500

    changed

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,10 +1,22 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import SuggestionInput from '@/components/SuggestionInput.vue';
 const input = ref(null);
+function noop() {
+
+}
+const items = reactive([
+  'Bill',
+  'Bob',
+  'Steve',
+  'Colin'
+]);
+const enteredText = ref('');
 </script>
 
 <template>
-<div class="container bg-red-200" @click.stop.prevent="input.focus()">
+<div class="container bg-red-200" @click="input.focus()" @mousedown.stop.prevent="">
   <input ref="input" type="text" class="border border-black">
 </div>  
+<SuggestionInput :items="items" v-model:enteredText="enteredText"/>
 </template>
