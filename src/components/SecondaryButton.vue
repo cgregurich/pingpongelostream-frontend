3d12d3b19de0b31896f81ae70c65c7787d156192
Colin Gregurich <coling112198@gmail.com>
2022-11-10 14:26:05 -0600

    changed colors

--- src/components/SecondaryButton.vue
+++ src/components/SecondaryButton.vue
@@ -14,9 +14,7 @@ defineProps({
 <template>
   <button
 
-    class="login-btn flex leading-none justify-center items-start bg-black text-white rounded-lg text-md transition-none px-4 py-3 h-min hover:bg-gray-800 active:bg-gray-700 
-
-    disabled:bg-gray-400 disabled:text-gray-500"
+    class="secondary-button flex leading-none justify-center items-start bg-site-color-one text-black border border-site-color-two rounded-lg text-md transition-none px-4 py-3 h-min hover:bg-gray-200 active:bg-gray-300 disabled:bg-gray-400 disabled:text-gray-500"
     :disabled="disabled"
   >
     {{ text }}
