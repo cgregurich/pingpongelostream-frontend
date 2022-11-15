826fbabb3888fa4b98c57a955137fce00f380385
Colin Gregurich <coling112198@gmail.com>
2022-11-14 20:04:43 -0600

    hooked up to backend, now has a singles/doubles selector

--- src/components/Leaderboard/Leaderboard.vue
+++ src/components/Leaderboard/Leaderboard.vue
@@ -3,151 +3,55 @@ import { ref, reactive, computed, onMounted } from 'vue';
 import LeaderboardCard from '@/components/Leaderboard/LeaderboardCard.vue';
 import LeaderboardHeader from '@/components/Leaderboard/LeaderboardHeader.vue';
 import LeaderboardFooter from '@/components/Leaderboard/LeaderboardFooter.vue';
+import axios from 'axios';
+import { API_URL } from '@/stores/utils/backendRouteParts.js';
 
-// const playerID = 2;
+const gameMode = ref('singles');
+const singlesTeams = reactive([]);
+const doublesTeams = reactive([]);
 onMounted(async () => {
-  /*
-  name
-  pic
-
-  
-  getPlayerSinglesRankingAndElo -> /api/players/{player_id}/teams/singles/ranking/season/{season_id}
-  elo
-  ranking
-
-  getTeamGames -> /api/teams/{teamid}/games/{seasonid}
-  games played
-  wins
-  losses
-  win rate
-  */
-
-  // const response = await axios.get(`${API_URL}/players/${playerID}/teams/singles/ranking/season/1`);
+  await fetchSinglesLeaderboard();
+  await fetchDoublesLeaderboard();
 });
 
-const players = reactive([
-  {
-    id: 1,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 1,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 2,
-    name: 'Bob',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 2,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 3,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 3,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 4,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 4,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 5,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 5,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 6,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 6,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 7,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 7,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 8,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 8,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 9,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 9,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-  {
-    id: 10,
-    name: 'David Johnston',
-    profilePhotoPath: 'https://xsgames.co/randomusers/assets/avatars/male/39.jpg',
-    ranking: 10,
-    elo: 2500,
-    gamesPlayed: 243,
-    gamesWon: 201,
-    gamesLost: 42,
-    winRate: 83
-  },
-]);
+async function fetchSinglesLeaderboard() {
+  const response = await axios.get(`${API_URL}/leaderboards/singles`);
+  // TODO: error checking
+  const fetchedTeams = response.data.response.teams;
+  Object.assign(singlesTeams, fetchedTeams);
+}
+
+async function fetchDoublesLeaderboard() {
+  const response = await axios.get(`${API_URL}/leaderboards/doubles`);
+  // TODO: error checking
+  const fetchedTeams = response.data.response.teams;
+  Object.assign(doublesTeams, fetchedTeams);
+}
+
+function gameModeChanged() {
+  if (gameMode.value === 'singles') {
+
+  }
+}
+
+const teams = computed(() => {
+  if (gameMode.value === 'singles') return singlesTeams;
+  else return doublesTeams;
+});
 
 </script>
 
 
 <template>
-<div class="leaderboard flex flex-col items-center">
+<div class="leaderboard flex flex-col items-center mx-4">
   <h1 class="text-4xl">Leaderboard</h1>
-  <p>Singles</p>
-  <table class="w-1/2 shadow-lg shadow-gray-400 rounded-lg">
+  <select v-model="gameMode" @change="gameModeChanged">
+    <option value="singles">Singles</option>
+    <option value="doubles">Doubles</option>
+  </select>
+  <table class="shadow-lg shadow-gray-400 rounded-lg">
     <LeaderboardHeader />
-    <LeaderboardCard v-for="player in players" :key="player.id" :player="player" />
+    <LeaderboardCard v-for="(team, index) in teams" :key="team.id" :team="team" :ranking="index + 1" />
     <LeaderboardFooter />
   </table>
 </div>
