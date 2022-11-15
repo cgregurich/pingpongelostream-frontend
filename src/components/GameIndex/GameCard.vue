6afdf0fdcb8b42429226db8773e90e0c15395c32
terrahQ <85520440+terrahQ@users.noreply.github.com>
2022-11-15 00:26:29 -0600

    Game page done without pagination

--- src/components/GameIndex/GameCard.vue
+++ src/components/GameIndex/GameCard.vue
@@ -3,92 +3,67 @@ import axios from "axios";
 import { reactive } from 'vue'
 import { API_URL, GAMES } from '@/stores/utils/backendRouteParts.js'
 import TeamCard from "./TeamCard.vue";
-import Score from "./Score.vue";
 // import Pagination from '../components/Pagination.vue'
 
 
 
 const games = reactive([]);
-const setsArray = reactive([]);
-const eachGame = reactive([]);
-const totalPages = 10;
-const currentPage = 1;
-const perPage = 6;
-let gameNum = 0;
-let page = 1;
+let winningTeamObject = "";
+let losingTeamObject = "";
+// const totalPages = 10;
+// const currentPage = 1;
+// const perPage = 6;
+// let gameNum = 0;
+// let page = 1;
 
-function onPageChange(page) {
-      console.log(page)
-      this.currentPage = page;
-    }
+// function onPageChange(page) {
+//       console.log(page)
+//       this.currentPage = page;
+//     }
 
 function formatDate(date) {
   const options = {year: 'numeric', month: 'long', day: 'numeric'}
   return new Date(date).toLocaleDateString('en', options)
 }
 
+/**
+ * Handles setting winningTeam
+ * TODO - also needs to handle more than 2 teams
+ */
 function getWinningTeam(game) {
-  let winner = game.team1_elo_change > game.team2_elo_change ? game.teams[0] : game.teams[1];
+  let winner = game.teams[0].pivot.set_score > game.teams[1].pivot.set_score ? game.teams[0] : game.teams[1];
   return winner;
 }
-
+/**
+ * Handles setting losingTeam
+ * TODO - also needs to handle more than 2 teams
+ */
 function getLosingTeam(game) {
-  let loser = game.team1_elo_change < game.team2_elo_change ? game.teams[0] : game.teams[1];
+  let loser = game.teams[0].pivot.set_score < game.teams[1].pivot.set_score ? game.teams[0] : game.teams[1];
   return loser;
 }
 
-// function getWinningScore(sets) {
-//   let scores1 = 1;
-//   let scores2 = 1;
-//   let wins = game.team1_elo_change > game.team2_elo_change ? game.teams[0] : game.teams[1];
-//   return wins;
-// }
 
-// function getLosingScore(sets) {
-//   let losses = game.team1_elo_change < game.team2_elo_change ? game.teams[0] : game.teams[1];
-//   return losses;
-// }
-
-const getSets = async() => {
-    for(let i = 1; i <= 10; i++) {
-        await getSet(i);
+/**
+ * Handles get request
+ * TODO - only handling season #1
+ */
+const getGames = async () => {
+try {
+  const response = await axios.get(API_URL + GAMES + "/season/1");
+  if(response.status === 200) {
+      const fetchedGames = response.data.response.games;
+      console.log(fetchedGames);
+      Object.assign(games, fetchedGames);
+      // handleScoreTeams(games.teams);
     }
-    // await console.log(setsArray);
+} catch (err) {
+  console.error(err);
 }
 
-const getSet = async (gameID) => {
-    try {
-    const response = await axios.get(API_URL + GAMES + "/" + gameID)
+}
 
-    if(response.status === 200) {
-      const fetchedOneGame = response.data.response.game;
-      // console.log(fetchedOneGame);game.teams[0]
-      eachGame.push(fetchedOneGame);
-      setsArray.push(fetchedOneGame.sets);
-      // console.log(setsArray);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-  }
-
-const getGamesRequest = async () => {
-  try {
-    const response = await axios.get(API_URL + GAMES)
-
-    if(response.status === 200) {
-      const fetchedGames = response.data.response.games;
-      Object.assign(games, fetchedGames);
-      // console.log(games);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-
-  };
-
-getGamesRequest();
-getSets();
+getGames();
 </script>
 
 <template>
@@ -98,28 +73,25 @@ getSets();
         flex flex-col
         sm:w-[60rem]
         w-full
-        bg-gray-100
         rounded-3xl
         m-6
         border border-gray-300
         shadow-lg
         items-center
+        bg-violet-100
       "
     >
       <p class="flex header text-4xl font-semibold m-4">Recent Games</p>
 
-      <div class="flex">
-          <ul>
-            <span class="flex flex-col w-full" v-for="game in games" :key="game.teams">
+      <div class="flex flex-col w-3/4">
+            <div class="flex flex-col w-full" v-for="game in games" :key="game.teams">
               <div class="flex flex-row self-center pb-1"><h1>{{ formatDate(game.completed_at) }}</h1></div>
-                <div class="flex flex-row w-full last:items-end border shadow-md rounded-xl py-2 px-4 lg:py-2 lg:px-10 bg-indigo-100 hover:bg-indigo-500">
+                <!-- <div class="flex flex-row w-full last:items-end border shadow-md rounded-xl py-2 px-4 lg:py-2 lg:px-10 bg-indigo-100 hover:bg-indigo-500"> -->
+                  <div class="flex flex-col border shadow-md rounded-xl  bg-gray-100 py-2 md:px-2 lg:px-5 sm:py-1 hover:shadow-gray-400">
                   <TeamCard :winningTeam=getWinningTeam(game) :losingTeam=getLosingTeam(game) />
-                  <Score :sets="setsArray[(game.id)-1]" />
                 </div>
-              <div class="self-center" v-if="gameNum !== perPage">___________________________</div> 
               <br/>
-            </span>
-          </ul>
+              </div>
       </div>
       <div
         class="line w-4/5 lg:mt-40 mt-1 bg-opacity-10 bg-black h-[1px] mb-4"
--- src/components/GameIndex/TeamCard.vue
+++ src/components/GameIndex/TeamCard.vue
@@ -5,18 +5,28 @@ const props = defineProps({
     losingTeam: Object
 })
 
+
+
+
 </script>
 
 <template>
-    <div class="flex-4 flex-col self-center px-3 hover:animate-ping">
-        <div class="flex-row font-semibold" v-for="member in winningTeam.members" :key="member.id">
-            {{ member.name }}
+    <div class="flex flex-row">
+        <div class="flex-1 flex-col self-center px-4 py-2">
+            <div class="flex-row text-right" v-for="member in props.winningTeam.members" :key="member.id">
+                {{ member.name }}
+            </div>
+        </div>
+        <div class="flex-2 flex-col self-center text-center">vs</div>
+        <div class="flex-1 flex-col self-center px-4 py-2">
+            <div class="flex-row" v-for="member in props.losingTeam.members" :key="member.id">
+                {{ member.name }}
+            </div>
         </div>
     </div>
-    <div class="flex-1 flex-col self-center text-center"><h1>bt</h1></div>
-    <div class="flex-4 flex-col self-center px-3">
-        <div class="flex-row" v-for="member in losingTeam.members" :key="member.id">
-            {{ member.name }}
-        </div>
+    <div class="flex flex-row self-center pb-2">
+        <div class="flex-row px-3 font-semibold text-center text-3xl">{{ props.winningTeam.pivot.set_score }}</div>
+        <div class="flex-row text-center text-3xl "> - </div>
+        <div class="flex-row px-3 text-center text-3xl">{{ props.losingTeam.pivot.set_score }}</div>
     </div>
 </template>
