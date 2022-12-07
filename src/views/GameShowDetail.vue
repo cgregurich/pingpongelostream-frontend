9da933b083a80a8dda9665fe6e1ac6d30ca998cf
Alex Wissing <awissing123@gmail.com>
2022-12-07 13:35:10 -0600

    fixed warning issues popping up in console on gameDetail and games index

--- src/views/GameShowDetail.vue
+++ src/views/GameShowDetail.vue
@@ -46,45 +46,48 @@ export default {
 
 <template>
 <div class="flex justify-center">
-  <div class="
-        flex flex-col
-        sm:w-[60rem]
-        rounded-3xl
-        m-10
-        p-5
-        border border-gray-300
-        shadow-lg
-        bg-white">
+  <div
+    class="
+      flex flex-col
+      sm:w-[60rem]
+      rounded-3xl
+      m-10
+      p-5
+      border border-gray-300
+      shadow-lg
+      bg-white"
+    v-if="game"
+  >
     <div class="flex justify-center pb-5" v-if="game.completed_at !== null">{{ formatDate(game.completed_at) }}</div>
     <div class="flex justify-center pb-5" v-else-if="(game.updated_at !== null)">{{ formatDate(game.updated_at) }}</div>
     <div class="flex justify-center pb-5" v-else-if="(game.started_at !== null)">{{ formatDate(game.started_at) }}</div>
     <div class="flex justify-center pb-5" v-else>TBD</div>
       <!-- Tennis style set scores in a box -->
-    <div class="border border-gray-300 rounded-md shadow-md" v-if="game"> 
-    <div class="flex p-4" v-for="team in game.teams" :key="team.id" :class="{'border-b border-gray-300' : game.teams.indexOf(team) == 0 }">
-      <div class="flex-1">
-        <div class="flex lg:text-3xl sm:text-2xl font-semibold p-1" v-for="member in team.members" :key="member.id">
-          <router-link class="flex" :to="{ name: 'Player', params: {id: member.id}}">
-            <img class="border border-grey-800 rounded-full w-11 shadow-xl" :src="member.profile_photo_url">
-            <div class="pl-3 hover:underline hover:cursor-pointer">
-              {{member.name}}
+    <div class="border border-gray-300 rounded-md shadow-md"> 
+      <div class="flex p-4" v-for="team in game.teams" :key="team.id" :class="{'border-b border-gray-300' : game.teams.indexOf(team) == 0 }">
+        <div class="flex-1">
+          <div class="flex lg:text-3xl sm:text-2xl font-semibold p-1" v-for="member in team.members" :key="member.id">
+            <router-link class="flex" :to="{ name: 'Player', params: {id: member.id}}">
+              <img class="border border-grey-800 rounded-full w-11 shadow-xl" :src="member.profile_photo_url">
+              <div class="pl-3 hover:underline hover:cursor-pointer">
+                {{member.name}}
+              </div>
+            </router-link>
+          </div>
+        </div>
+        <div class="flex-1 lg:text-5xl sm:text-4xl self-center font-normal text-gray-700">
+          <div class="flex" v-if="game.teams.indexOf(team) == 0">
+            <div class="flex-1 text-center" :class="{ 'text-black font-semibold' : set.team1_score > set.team2_score}" v-for="set in game.sets" :key="set.id">
+                {{set.team1_score}}
             </div>
-          </router-link>
-        </div>
-      </div>
-      <div class="flex-1 lg:text-5xl sm:text-4xl self-center font-normal text-gray-700">
-        <div class="flex" v-if="game.teams.indexOf(team) == 0">
-          <div class="flex-1 text-center" :class="{ 'text-black font-semibold' : set.team1_score > set.team2_score}" v-for="set in game.sets" :key="set.id">
-              {{set.team1_score}}
           </div>
-        </div>
-        <div class="flex" v-if="game.teams.indexOf(team) == 1">
-          <div class="flex-1 text-center" :class="{ 'text-black font-semibold' : set.team2_score > set.team1_score}" v-for="set in game.sets" :key="set.id">
-              {{set.team2_score}}
+          <div class="flex" v-if="game.teams.indexOf(team) == 1">
+            <div class="flex-1 text-center" :class="{ 'text-black font-semibold' : set.team2_score > set.team1_score}" v-for="set in game.sets" :key="set.id">
+                {{set.team2_score}}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
     <!-- Wordy part. TODO: add leaderboard info -->
     <GameShowContent v-if="game"
